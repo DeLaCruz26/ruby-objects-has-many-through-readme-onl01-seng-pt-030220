a949590620db51bed6491c0986228e75e153119e
Dametrik Fick <dametrik.fick@gmail.com>
2020-04-06 04:07:33 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -18,4 +18,10 @@ attr_accessor :name, :yrs_experience
     Meal.new(self, customer, total, tip)
   end
 
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+
 end
