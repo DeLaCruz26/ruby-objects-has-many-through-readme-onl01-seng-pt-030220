b42a63cd2731e70298a913f46a3a2cc906a40991
Dametrik Fick <dametrik.fick@gmail.com>
2020-04-06 04:00:22 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,7 +15,10 @@ attr_accessor :name, :age
   end
 
   def new_meal(waiter, total, tip=0)
-    Meal .new(waiter, self, total, tip)
+    Meal.new(waiter, self, total, tip)
   end
+
+  def meals
+    Meal.all
 
 end
